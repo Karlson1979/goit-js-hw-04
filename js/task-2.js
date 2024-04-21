de93b0bd88861ca8fe34d13acce2981b94a3b3c6
Tochka <tochka114@gmail.com>
2024-04-21 22:16:49 +0300

    task2

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+function calcAverageCalories(days){
+    let sumCalories=0
+    for(const day of days){
+        sumCalories+=day.calories
+
+    }
+    return  sumCalories/7
+}
+console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 3010 },
+      { day: "tuesday", calories: 3200 },
+      { day: "wednesday", calories: 3120 },
+      { day: "thursday", calories: 2900 },
+      { day: "friday", calories: 3450 },
+      { day: "saturday", calories: 3280 },
+      { day: "sunday", calories: 3300 }
+    ])
+  ); // 3180
